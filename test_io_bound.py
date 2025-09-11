0f418f248fb5acd7357361a1d72f140fd309aec8
tz <tz@192.168.124.5>
2025-09-11 11:37:09 +0800

    rearranged test_io_bound.py

--- test_io_bound.py
+++ test_io_bound.py
@@ -8,21 +8,6 @@ def write_file_block(filename, block_size=1024*1024*100):
         f.write(data)
 
 N = 5  # number of files
-start = time.time()
-
-# Ensure test files are removed
-for i in range(N):
-    try:
-        os.remove(f"file_{i}.bin")
-    except FileNotFoundError:
-        pass
-
-with finish():
-    for i in range(N):
-        async_(lambda i=i: write_file_block(f"file_{i}.bin"))
-
-print("Total time (with async-finish):", time.time() - start)
-
 
 start = time.time()
 # Ensure test files are removed
@@ -35,3 +20,18 @@ for i in range(N):
 for i in range(N):
     write_file_block(f"file_{i}.bin")
 print("Total time (no async-finish):", time.time() - start)
+
+
+start = time.time()
+# Ensure test files are removed
+for i in range(N):
+    try:
+        os.remove(f"file_{i}.bin")
+    except FileNotFoundError:
+        pass
+
+with finish():
+    for i in range(N):
+        async_(lambda i=i: write_file_block(f"file_{i}.bin"))
+
+print("Total time (with async-finish):", time.time() - start)
